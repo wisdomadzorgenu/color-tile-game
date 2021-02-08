3461102fd46256b41b6d201d84bdedea8dd32392
Wisdom Adzorgenu <wadzorgenu@gmail.com>
2021-02-08 10:06:56 +0000

    changed backend server to point to right url.

--- frontend/src/config/serverUrl.ts
+++ frontend/src/config/serverUrl.ts
@@ -1,4 +1,4 @@
 const SERVER_URL = "http://localhost:5050";
-// const SERVER_URL = "https://ever-real-test-wisdom.nw.r.appspot.com";
+// const SERVER_URL = "https://color-tile-game-backend.oa.r.appspot.com";
 
 export {SERVER_URL};
